4a7f8ab0be9ee69846e92e1d7153a94ff9b1d41a
Luffyt01 <luffytaaro112@gmail.com>
2025-12-15 19:45:41 +0530

    Rebase 1

---
+++ Backend/user_service/src/main/java/com/project/user_service/dto/LogInDto.java
@@ -0,0 +1,9 @@
+package com.project.user_service.dto;
+
+import lombok.Data;
+
+@Data
+public class LogInDto {
+    private String email;
+    private String password;
+}
---
+++ Backend/user_service/src/main/java/com/project/user_service/entities/UserEntity.java
@@ -0,0 +1,73 @@
+package com.project.user_service.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.user_service.entities.enums.UserRole;
+import com.project.user_service.entities.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name= "user_table",
+   indexes = {
+   @Index(name = "idx_email",columnList = "email",unique = true)
+
+   }
+
+)
+
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+
+    private String FullName;
+    private String email;
+    private String password;
+    private Number phone;
+
+    @Enumerated(EnumType.STRING)
+    private Set<UserRole> roles;
+
+    private LocalDateTime createAt;
+
+    private Status status;
+    @JsonProperty
+    private boolean email_verified;
+    @JsonProperty
+    private  boolean profile_complete;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updateAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role->new SimpleGrantedAuthority("ROLE_"+role.name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
