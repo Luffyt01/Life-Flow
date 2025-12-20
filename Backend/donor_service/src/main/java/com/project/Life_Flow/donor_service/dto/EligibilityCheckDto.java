a6a65bc536b397cb7be9027a259968bf4836eb4a
Luffyt01 <luffytaaro112@gmail.com>
2025-12-20 23:59:08 +0530

    Implemented Donor profile logic

--- Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/dto/EligibilityCheckDto.java
+++ Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/dto/EligibilityCheckDto.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class EligibilityCheckDto {
     private UUID checkId;
-    private UUID donor;
+    private UUID donorId;
     private LocalDateTime checkTimestamp;
     private Integer lastDonationDays;
     private BigDecimal minWeightKg;
