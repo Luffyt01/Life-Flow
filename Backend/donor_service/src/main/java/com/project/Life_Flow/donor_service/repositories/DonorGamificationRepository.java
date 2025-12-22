e8cc40c16caabeef93e8e6fb89b9b66fc36a951c
Luffyt01 <luffytaaro112@gmail.com>
2025-12-22 13:12:09 +0530

    Updated donor service

--- Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/repositories/DonorGamificationRepository.java
+++ Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/repositories/DonorGamificationRepository.java
@@ -1,12 +1,15 @@
 package com.project.Life_Flow.donor_service.repositories;
 
 import com.project.Life_Flow.donor_service.entities.DonorGamification;
+import com.project.Life_Flow.donor_service.entities.DonorProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DonorGamificationRepository extends JpaRepository<DonorGamification, UUID> {
 
+    Optional<DonorGamification> findByDonor(DonorProfile donor);
 }
