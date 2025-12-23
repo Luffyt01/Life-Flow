bd814417b07db8630b5fb16f7aa3cbb84bb819f7
Luffyt01 <luffytaaro112@gmail.com>
2025-12-23 23:58:14 +0530

    Rebase 7

--- Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/services/DonorService.java
+++ Backend/donor_service/src/main/java/com/project/Life_Flow/donor_service/services/DonorService.java
@@ -2,22 +2,13 @@ package com.project.Life_Flow.donor_service.services;
 
 import com.project.Life_Flow.donor_service.dto.DonorProfileDto;
 import com.project.Life_Flow.donor_service.dto.DonorProfileRequestDto;
-import com.project.Life_Flow.donor_service.dto.DonorProfileResponse;
-import com.project.Life_Flow.donor_service.dto.EligibilityCheckResponse;
 import com.project.Life_Flow.donor_service.entities.DonorProfile;
-import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.UUID;
 
 public interface DonorService {
 
-    DonorProfileResponse getMyProfile(UUID userId);
+    DonorProfileDto getMyProfile(UUID donorId);
 
-    DonorProfileResponse registerDonor(DonorProfileRequestDto request, UUID userId);
-
-    DonorProfileResponse getDonorProfile(UUID donorId);
-
-    EligibilityCheckResponse checkEligibility(UUID donorId);
-
-    DonorProfile getDonorEntity(UUID donorId);
+    DonorProfileDto createDonorProfile(DonorProfileRequestDto donorProfileRequestDto);
 }
