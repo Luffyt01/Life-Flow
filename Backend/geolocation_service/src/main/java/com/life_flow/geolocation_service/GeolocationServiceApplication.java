0c2c44442db3fd6161aee2913e7f91e4017c8f33
Rocky sheoran <rockysheoran72@gmail.com>
2026-01-10 15:51:03 +0530

    add some more thing in service

--- Backend/geolocation_service/src/main/java/com/life_flow/geolocation_service/GeolocationServiceApplication.java
+++ Backend/geolocation_service/src/main/java/com/life_flow/geolocation_service/GeolocationServiceApplication.java
@@ -2,8 +2,11 @@ package com.life_flow.geolocation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+
 public class GeolocationServiceApplication {
 
 	public static void main(String[] args) {
