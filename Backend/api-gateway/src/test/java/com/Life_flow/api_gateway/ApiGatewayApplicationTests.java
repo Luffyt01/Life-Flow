c13e2d4f0ade1505d7135df07c6aeb303fb1ea2c
Rocky sheoran <rockysheoran72@gmail.com>
2026-01-10 13:50:24 +0530

    added api gateway and discovery service

---
+++ Backend/api-gateway/src/test/java/com/Life_flow/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Life_flow.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
