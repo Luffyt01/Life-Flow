7cf929d8450bef5b8de3ec20a710d939502acdc9
Rocky sheoran <rockysheoran72@gmail.com>
2026-01-16 16:05:05 +0530

    user service remove from dev1 branch

--- Backend/user_service/src/main/java/com/project/user_service/entities/enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.project.user_service.entities.enums;
-
-public enum UserRole {
-    DONOR,
-    HOSPITAL,
-    ADMIN
-}
--- Backend/user_service/src/main/java/com/project/user_service/exception/ExceptionType/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.project.user_service.exception.ExceptionType;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
